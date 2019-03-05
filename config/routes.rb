667b9acecb14868217458f60f0e6a3663cdba749
Hiroshi Ueki <hirocueki@gmail.com>
2019-03-05 12:02:13 +0900

    ルーティングの修正とレイアウトの修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   root to: "questions#index"
   devise_for :users
 
-  resources :user do
+  resources :questions, only: %i(index show)
+  resources :tags, only: %i(index show)
+
+  resources :users do
     resources :questions
   end
 end
