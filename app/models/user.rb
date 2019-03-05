6b902bbb7f10b647bdcd2fb0d7e754be2377e522
Hiroshi Ueki <hirocueki@gmail.com>
2019-03-05 15:01:50 +0900

    acts_as_ordered_taggableに変更。dependentの構文ミスの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :questions
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
+
+  has_many :questions, dependent: :destroy
 end
