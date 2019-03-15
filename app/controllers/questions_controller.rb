fffc876ba52a7418c654092f8c5a69a87f11667f
Hiroshi Ueki <hirocueki@gmail.com>
2019-03-15 23:13:23 +0900

    回答のルーティングとバグ修正

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,6 @@
 class QuestionsController < ApplicationController
   skip_before_action :authenticate_user!
-  before_action :set_question, only: :show
+  before_action :set_question, only: %i(show edit update destroy)
 
   def index
     @questions = Question.all.includes(:tags).order(created_at: :desc)
@@ -8,13 +8,49 @@ class QuestionsController < ApplicationController
 
   def show
     @answer = Answer.new
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def edit
     @user = current_user
+  end
+
+  def create
+    @question = current_user.questions.build(question_params)
+
+    if @question.save
+      redirect_to @question, notice: "質問「#{@question.title}」を作成しました"
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to @question, notice: "質問「#{@question.title}」を更新しました"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @question.destroy
+    redirect_to questions_url, notice: "質問「#{@question.title}」を削除しました"
   end
 
   def tagged
     @user ||= current_user if user_signed_in?
     @tag_name = params[:tagname]
     @questions = Question.tagged_with(@tag_name).order(created_at: :desc)
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :content, :tag_list)
   end
 
   private
