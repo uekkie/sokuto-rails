baff5fd6e37eba14c612a3ebe616a8380edc6158
Hiroshi Ueki <hirocueki@gmail.com>
2019-04-05 19:27:01 +0900

    直す箇所をコメント

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,6 +2,7 @@ class QuestionsController < ApplicationController
   before_action :set_question, only: %i(show edit update destroy)
 
   def index
+    # TODO: スマートに書き直したい
     @questions = Question.all.includes(:user).order(created_at: :desc)
     @weighted_total_questions = Question.all.order(cached_weighted_total: :desc)
     @no_answers_questions = Question.all.where(answers_count: 0).order(created_at: :desc)
@@ -12,6 +13,7 @@ class QuestionsController < ApplicationController
   end
 
   def tagged
+    # TODO: tag_nameにする
     @tag_name = params[:tagname]
     @questions = Question.tagged_with(@tag_name).order(created_at: :desc)
   end
