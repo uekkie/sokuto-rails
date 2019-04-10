4c2188d494805d600584c7a7e6690e64fb11846b
Hiroshi Ueki <hirocueki@gmail.com>
2019-04-10 19:15:26 +0900

    TODOの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  # TODO: user名以外にプロフィールの情報（居住地、スコア、得意分野）を表示する
   def index
     @q = User.ransack(params[:q])
     @users = @q.result.page(params[:page])
