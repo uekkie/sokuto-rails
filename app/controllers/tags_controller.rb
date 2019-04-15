d8adbe0980531e38aef42ee7d3a41190a2997472
Hiroshi Ueki <hirocueki@gmail.com>
2019-04-15 21:19:53 +0900

    TODOを追加

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,6 @@
 class TagsController < ApplicationController
   def index
+    # TODO: 人気タブ、名前タブ、新規タブを実装する
     @q = ActsAsTaggableOn::Tag.ransack(params[:q])
     @tags = @q.result.page(params[:page])
   end
