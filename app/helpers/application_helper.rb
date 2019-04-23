03c430a26dcd89c90eaa74eea87271f86debb484
Hiroshi Ueki <hirocueki@gmail.com>
2019-04-23 16:17:39 +0900

    アバターの作成部分をヘルパーにしてDRYにした

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def is_active_params(param_name)
     @sort_type == param_name ? "active" : ""
   end
+
+  def avatar_image(user, **class_name)
+    image_tag InitialAvatar.avatar_data_uri(user.name.first), class_name
+  end
 end
