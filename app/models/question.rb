f80dfc7444d0bd9cdfcc647b61d9da5610d0aa1e
Hiroshi Ueki <hirocueki@gmail.com>
2019-04-24 22:14:50 +0900

    time_ago_in_wordsをつかって日付周りの処理を簡潔にした

--- app/models/question.rb
+++ app/models/question.rb
@@ -14,13 +14,4 @@ class Question < ApplicationRecord
   def truncated_content
     content.truncate(40)
   end
-
-  def humanize_created_at
-    seconds = (Time.now - created_at).round;
-    days = seconds / (60 * 60 * 24); return I18n.l(created_at, format: :short) if days > 0
-    hours = seconds / (60 * 60); return "#{hours}時間前" if hours > 0
-    minutes = seconds / 60; return "#{minutes}分前" if minutes > 0
-
-    return "#{seconds}秒前"
-  end
 end
