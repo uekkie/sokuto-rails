86aa5d4d7ed4b7c0c369e341e966414ee460adc0
Hiroshi Ueki <hirocueki@gmail.com>
2019-04-25 00:03:11 +0900

    Tagモデルを実装し、kaminariのページネイトをカスタマイズ

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,6 @@
 class TagsController < ApplicationController
   def index
-    @q = ActsAsTaggableOn::Tag.ransack(params[:q])
+    @q = Tag.ransack(params[:q])
     @tags = @q.result.page(params[:page])
   end
 end
---
+++ app/models/tag.rb
@@ -0,0 +1,3 @@
+class Tag < ActsAsTaggableOn::Tag
+  paginates_per 12
+end
