b26dabf4b61843144c0a1688674d86a91312ebd2
Hiroshi Ueki <hirocueki@gmail.com>
2019-04-29 12:21:29 +0900

    fix

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -26,38 +26,4 @@ RSpec.feature "Users", type: :system do
 
     expect(page).to have_content 'ログインしました'
   end
-    #
-    # scenario "ユーザーはログアウトができる" do
-    #   sign_in user
-    #   visit root_path
-    #   expect(page).to have_content "ログアウト"
-    #
-    #   click_link 'ログアウト'
-    #
-    #   expect(page).to have_content 'ログアウトしました'
-    #
-    #   expect(current_path).to eq(root_path)
-    # end
-    #
-    #
-    # scenario "ユーザーはあたらしい投稿を作成することができる" do
-    #   sign_in user
-    #   visit new_user_item_path(user)
-    #
-    #   click_link '投稿する'
-    #   fill_in 'item_title', with: 'Test Item'
-    #   fill_in 'item_tag', with: 'Ruby,Rails'
-    #   fill_in 'item_body', with: 'Trying out Capybara'
-    #
-    #   expect {
-    #     click_button '投稿する'
-    #   }.to change(user.items, :count).by(1)
-    #
-    #   expect(page).to have_content '作成しました'
-    #   expect(page).to have_content 'Test Item'
-    #   expect(page).to have_content 'Ruby'
-    #   expect(page).to have_content 'Rails'
-    #   expect(page).to have_content 'Trying out Capybara'
-    # end
-
 end
