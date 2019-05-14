6a6aca13cac72bfac89f8ad23749e593b91ea992
Hiroshi Ueki <hirocueki@gmail.com>
2019-05-14 23:08:41 +0900

    使用していないコードの削除

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,4 @@
 module ApplicationHelper
-  def is_active_params(param_name)
-    @sort_type == param_name ? "active" : ""
-  end
-
   def avatar_image(user, **options)
     image_tag InitialAvatar.avatar_data_uri(user.name.first), options
   end
