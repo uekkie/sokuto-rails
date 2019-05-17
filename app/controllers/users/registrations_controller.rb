e5427fe6c15f20da609422d7494e9eca44975fc6
Hiroshi Ueki <hirocueki@gmail.com>
2019-05-17 16:05:20 +0900

    ユーザー情報の更新をUsers::RegistrationsControllerに移譲

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,11 +3,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
 private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-      user_params.permit(:name, :email, :password, :password_confirmation)
-    end
-    devise_parameter_sanitizer.permit(:account_update) do |user_params|
-      user_params.permit(:name, :email, :password_confirmation, :current_password)
-    end
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 end
