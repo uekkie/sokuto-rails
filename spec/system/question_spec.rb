6307839282cf72d39b2bd574647593c61be21422
Hiroshi Ueki <hirocueki@gmail.com>
2019-07-16 23:39:46 +0900

    テストの修正.マークアップの変化に左右されにくい形式に書き換え

--- spec/system/question_spec.rb
+++ spec/system/question_spec.rb
@@ -11,14 +11,14 @@ RSpec.feature "Questions", type: :system do
       visit root_path
       click_on '質問する'
 
-      fill_in 'question[title]', with: 'プログラマーにとって大切なことはなんですか'
-      fill_in 'question[tag_list]', with: '職業, プログラマー, 考え方'
-      fill_in 'question[content]', with: 'プロのプログラマーを続けていく上で、あなたが大切にしている考えや、心がけていることなどあれば教えてください。'
+      fill_in 'タイトル', with: 'プログラマーにとって大切なことはなんですか'
+      fill_in 'タグ（カンマで区切ります）', with: '職業, プログラマー, 考え方'
+      fill_in '質問の内容', with: 'プロのプログラマーを続けていく上で、あなたが大切にしている考えや、心がけていることなどあれば教えてください。'
 
       click_on '投稿する'
 
       expect(page).to have_content '質問「プログラマーにとって大切なことはなんですか」を作成しました'
-      expect(page).to have_selector 'h1.title', text: 'プログラマーにとって大切なことはなんですか'
+      expect(page).to have_selector "[data-test='question_title']", text: 'プログラマーにとって大切なことはなんですか'
       expect(page).to have_selector '.tags', text: '職業 プログラマー 考え方'
       expect(page).to have_selector '.content', text: 'プロのプログラマーを続けていく上で、あなたが大切にしている考えや、心がけていることなどあれば教えてください。'
     end
