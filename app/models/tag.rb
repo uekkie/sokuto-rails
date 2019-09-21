5f734581bfac223f52564fc47e3796afe0d0cb23
Hiroshi Ueki <hirocueki@gmail.com>
2019-09-21 21:47:56 +0900

    タグを新規順に並び替えるように
    acts_as_taggableがAR6を正式サポートしていないため、rails 5.2.3にダウングレードしました

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,13 +1,15 @@
 class Tag < ActsAsTaggableOn::Tag
   paginates_per 12
 
+  extend OrderAsSpecified
+
   scope :query_with, ->(type) {
     case type
     when 'new'
       # 質問の作成日順に
-      # newer_tag_ids = Question.newer_tag_ids
       newer_tag_ids = Question.all.order(created_at: :desc).includes(:tags).pluck(:tag_id).compact.uniq
-      where(id: [1, 3, 2]).order(["field(id, ?)", [1, 3, 2]])
+      Tag.order_as_specified(id: newer_tag_ids)
+
     when 'name'
       order(name: :asc)
     else
