425a7f061dfa2518d567275054412dfbb5eaa158
Hiroshi Ueki <hirocueki@gmail.com>
2019-09-30 00:07:42 +0900

    テスト追加

--- spec/system/home_spec.rb
+++ spec/system/home_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 
-describe 'ホームページ' do
+describe 'ホームページ', type: :system do
+  let!(:user) { create(:user) }
+
   before do
     driven_by :selenium_chrome_headless
   end
@@ -13,9 +15,14 @@ describe 'ホームページ' do
 
   it '人気、今週、今月のタブが存在すること' do
     visit root_url
-
     expect(page).to have_content '人気'
     expect(page).to have_content '今週'
     expect(page).to have_content '今月'
+  end
+
+  it 'ログインしていれば「質問する」ボタンがあること' do
+    sign_in user
+    visit root_url
+    expect(page).to have_content '質問する'
   end
 end
