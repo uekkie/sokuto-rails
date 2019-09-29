01762f5de00ec4c834af3747249d6e719376d7db
Hiroshi Ueki <hirocueki@gmail.com>
2019-09-29 23:26:43 +0900

    ロジカルな記述をやめた

--- spec/system/tag_spec.rb
+++ spec/system/tag_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 RSpec.feature "Tags", type: :system do
   it '人気、名前、新規のタブが存在すること' do
     visit tags_url
-    %w(人気 名前 新規).each { |content|
-      expect(page).to have_content content
-    }
+
+    expect(page).to have_content '人気'
+    expect(page).to have_content '名前'
+    expect(page).to have_content '新規'
   end
 end
