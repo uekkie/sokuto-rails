47ac0531f1c89425b6da6f1ce4bc8fb9b7a3d0c7
Hiroshi Ueki <hirocueki@gmail.com>
2019-10-06 15:16:32 +0900

    タグに関するスペックを追加

---
+++ spec/factories/questions.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :question do
+    title { 'プログラマーの方に質問です。正直、1日8時間も集中力続かなくないですか？' }
+    content { 'プログラマーの方に質問です。正直、1日8時間も集中力続かなくないですか？' }
+  end
+
+end
--- spec/system/tag_spec.rb
+++ spec/system/tag_spec.rb
@@ -8,4 +8,26 @@ RSpec.feature "Tags", type: :system do
     expect(page).to have_content '名前'
     expect(page).to have_content '新規'
   end
+
+  let!(:question) {
+    user = create(:user)
+    create(:question, user: user, tag_list: ['働き方'])
+  }
+
+  it '質問についたタグがタグ一覧に追加される' do
+    visit tags_path
+    expect(page).to have_content '働き方 × 1'
+  end
+
+  it '質問の詳細ページにタグがある' do
+    visit question_path(question)
+    expect(page).to have_content '働き方'
+  end
+
+  it 'おなじタグをつけたときはタグ一覧の数が増える' do
+    user = create(:user)
+    create(:question, user: user, tag_list: ['働き方'])
+    visit tags_path
+    expect(page).to have_content '働き方 × 2'
+  end
 end
