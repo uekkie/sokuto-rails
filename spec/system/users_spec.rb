d6fa5bf74ca4849b58bfe11233e99f8c6ab5ae27
Hiroshi Ueki <hirocueki@gmail.com>
2019-10-06 22:19:24 +0900

    ユーザースペックの追加

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -34,4 +34,33 @@ describe "Users", type: :system do
 
     expect(page).to have_content 'ログインしました'
   end
+
+  context 'ユーザー検索したとき' do
+    before do
+      create(:user, name: 'リーチマイケル')
+      create(:user, name: '中島イシレリ')
+      create(:user, name: '松島幸太郎')
+    end
+
+    it 'ヒットしたユーザーだけが表示される' do
+      visit users_path
+      fill_in '名前', with: 'リーチ'
+      click_on '絞り込む'
+
+      within '.users-list' do
+        user_names = all('.user').map(&:text)
+        expect(user_names).to eq ['リーチマイケル']
+      end
+    end
+    it 'ヒットしなかった' do
+      visit users_path
+      fill_in '名前', with: 'グレイグ'
+      click_on '絞り込む'
+
+      within '.users-list' do
+        user_names = all('.user').map(&:text)
+        expect(user_names).to be_empty
+      end
+    end
+  end
 end
